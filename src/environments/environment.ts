0e5ee71df0d9eb2feff3c9e9ecf66295fb7a81ff
pablo schmitt <pablo.schmitt@hotmail.com.ar>
2026-02-25 21:39:21 -0300

    x

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   authConfig: {
-    authority: 'https://localhost:7301',
-    //authority: 'https://sb-idp.malvinasargentinas.gob.ar',
+    //authority: 'https://localhost:7301',
+    authority: 'https://sb-idp.malvinasargentinas.gob.ar',
   },
   externalSites: {
     habilitacionesSite: 'https://localhost:4205/datos/rodado/1306?v=RC-12-12345679',
